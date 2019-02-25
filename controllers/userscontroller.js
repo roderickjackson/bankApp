647957eb16c9bd0302b40eef70343ecc0fb90d19
Roderick <notmine@rjbradley.local>
2019-02-25 15:31:56 -0600

    Complete user controller. create route, then create passport.js

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -1,7 +1,56 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+// Input validation
+const validateRegisterInput = require('../validation/register')
+const validateLoginInput = require('../validation/login')
+
+// User Model
 const User =  require('../models/user');
 
 
-module.exports = { index}
+module.exports = { index, create}
+
+function create(req, res){
+    /*
+     * form validation
+     */
+    const {errors, isValid} = validateRegisterInput(req.body)
+    /*
+     * Check validation
+     */
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+    
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if(user){
+            return res.status(400).json({email: "Email aleady exists"})}
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    /*
+     * Hash password before saving in database
+     */
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if(err) throw err;
+                newUser.password = hash
+                newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err))
+            })
+        })
+    })
+
+}
+
+
 
 function index(req, res){
     User.find({}, (err, users) => {res.render('users/index', {users})})
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs')
+
 const usersRouter = require('../controllers/userscontroller')
 
 
 /* GET users listing. */
 router.get('/', usersRouter.index)
+router.post('/', usersRouter.create)
 // Router.get('/flights/new', usersController.new)
 // Router.get('/:id', usersController.show)
 // Router.post('/flights/new', usersController.create);
