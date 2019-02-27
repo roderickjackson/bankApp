f84e6f071d7db4d2b8e3006b6ee6df6607222fa6
Roderick <notmine@rjbradley.local>
2019-02-26 18:19:55 -0600

    combin error and auth reducer, then link redux to react components

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,82 @@
+import axios from 'axios'
+import setAuthToken from '../utils/setAuthToken'
+import jwt_decode from 'jwt-decode'
+
+import {
+    GET_ERRORS,
+    SET_CURRENT_USER,
+    USER_LOADING,
+} from './types'
+
+// Register User
+export const registerUser = (userData, history) => {
+    axios
+        // I will change this to /api/users/register when I get a chance
+        .post('/users/register', userData)
+        // Re-direct user to login on successful register
+        .then(res => history.push('/login'))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+// Login - get user token
+export const loginUser = userData => dispatch => {
+    axios  
+         // I will change this to /api/users/login when I get a chance
+        .post('/users/login', userData)
+        .then(res => {
+            // Save to sessionStorage
+
+            // Set token to sessionStorage
+            const {token} = res.data
+            sessionStorage.setItem('jwtToken', token)
+            
+            // Set token to Auth header
+            setAuthToken(token)
+
+            // Decode token to get user data
+            const decoded = jwt_decode(token)
+
+            // Set current user 
+            // look further down and you'll see where 
+            // setCurrentUser is set up as a variable to
+            // accept the SET_CURRENT_USER type
+            dispatch(setCurrentUser(decoded))
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+    return{
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+// User loading
+export const setUserLoading = () => {
+    return{
+        type: USER_LOADING
+    }
+}
+
+// Log user out
+export const logoutUser = () => {
+    // Remove token from local storage
+    sessionStorage.removeItem('jwtToken')
+    
+    // Remove auth header for future requests
+    setAuthToken(false)
+
+    // Set current user to empty object {} which will
+    // set isAuthenticate to false
+    dispatch(setCurrentUser({}))
+}
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const setAuthToken = token => {
+    if(token){
+        // Apply authorization token to every request if logged in 
+        // Ok so I'm taking a lucky guess with this,
+        // This piece of code is going to place the token to the
+        // XMLHttpRequest header on every request
+        // We want this token so the user can refresh the browser
+        // And still be logged in on both browser and mobile devices
+        axios.defaults.headers.common = token
+    }
+    else{
+        // Delete auth header
+        delete axios.defaults.headers.common['Authorization']
+    }
+}
+
+export default setAuthToken
